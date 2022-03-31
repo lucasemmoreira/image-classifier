4f67b0f16778744e3fb238cf49f3263417fdbd71
Lucas Moreira <lucasemmoreira@gmail.com>
2022-03-30 21:26:46 -0300

    change: requirements, and pep 8

--- train_food.py
+++ train_food.py
@@ -6,9 +6,9 @@ from time import time
 from PIL import Image
 
 
-data_dir = "./data/food-5"
+data_dir = "../data/images/"
 
-im = Image.open("./data/food-5/apple_pie/1005649.jpg")
+im = Image.open("../data/images/apple_pie/1005649.jpg")
 im_shape = im.size
 img_height = im_shape[0]
 img_width = im_shape[1]
@@ -33,32 +33,32 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size=batch_size)
 
 data_augmentation = keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-    layers.experimental.preprocessing.RandomRotation(0.1),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal",
+                                                     input_shape=(img_height,
+                                                                  img_width,
+                                                                  3)),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        layers.experimental.preprocessing.RandomZoom(0.1),
+    ]
 )
 
 num_classes = 5
 
 
 model = Sequential([
-  data_augmentation,
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+    data_augmentation,
+    layers.experimental.preprocessing.Rescaling(1./255),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
 ])
 
 model.compile(
@@ -69,10 +69,11 @@ model.compile(
 
 print("start training! go go go!")
 epochs = 15
+
 history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
 )
 print("time to train ", time() - start)
 
